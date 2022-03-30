d3b35437d49c22b80c706944d5f124a5c68fdff0
lu wagner <lu.wagner91@gmail.com>
2022-03-30 20:12:14 -0300

    Inicializacion de proyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const path = require ("path");
+const app = express();
+
+//Le decimos que puede devolver todo lo que esté en public, si es pedido.
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+
+app.listen(3000, () => console.log("servidor en http://localhost:3000/"));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
