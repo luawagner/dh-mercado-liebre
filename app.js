d5d8f1aad7ad79415588ff1b3eae399dd3a88b95
lu wagner <lu.wagner91@gmail.com>
2022-03-30 21:11:19 -0300

    add heroku config

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express= require("express");
 const path = require ("path");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 //Le decimos que puede devolver todo lo que esté en public, si es pedido.
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => console.log("servidor en http://localhost:3000/"));
+app.listen(PORT, () => console.log("servidor en http://localhost:3000/"));
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
